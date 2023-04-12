b58470053de7998e9e1080fdcf7a9e0a154678d9
yuanfei <1574133487@qq.com>
2023-04-12 18:32:22 +0800

    feat: 使用lint-staged

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,7 +4,7 @@ import { navigateToPage } from "@yaunfei/taro-router";
 export default function Index() {
   return (
     <View>
-      <Text>Hello world!</Text>
+      <Text>Hello world!2222</Text>
       <Button
         onClick={() => {
           navigateToPage("/pages/common/error/index", {}, {});
